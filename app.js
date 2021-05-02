2f52c2e3d34a52fec01a2db1116b2634beec4156
omh02033 <omh02033@gmail.com>
2021-05-02 14:15:36 +0900

    add: get Device unique code

---
+++ app.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(cookieParser());
+
+app.use('/', require('./routes/index.js'));
+
+app.listen(333, () => { console.log("Connected !") });
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router
+.get('/', (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, '../public/html') });
+});
+
+module.exports = router;
