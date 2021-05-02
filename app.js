ef096fb550d2d770b9b5dc8d3983f4bba6fab488
omh02033 <omh02033@gmail.com>
2021-05-02 14:24:03 +0900

    feat: test port

--- app.js
+++ app.js
@@ -15,4 +15,4 @@ app.use(cookieParser());
 
 app.use('/', require('./routes/index.js'));
 
-app.listen(333, () => { console.log("Connected !") });
+app.listen(8080, () => { console.log("Connected !") });
